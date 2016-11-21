2282d7156f0795c61ebf0d74a25026935d277b52
Maarten Van Giel <maarten.vangiel@icapps.com>
2016-11-21 13:06:58 +0100

    Call onWindowVisible and onWindowInvisible on tool window ancestor change

--- niddler-plugin/src/com/icapps/niddler/ui/NiddlerToolWindow.kt
+++ niddler-plugin/src/com/icapps/niddler/ui/NiddlerToolWindow.kt
@@ -5,9 +5,14 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
+import com.intellij.ui.content.ContentManagerEvent
+import com.intellij.ui.content.ContentManagerListener
+import javax.swing.event.AncestorEvent
+import javax.swing.event.AncestorListener
 
 /**
  * @author Nicola Verbeeck
+ *
  * @date 21/11/16.
  */
 class NiddlerToolWindow : ToolWindowFactory {
@@ -17,8 +22,24 @@ class NiddlerToolWindow : ToolWindowFactory {
     override fun createToolWindowContent(p0: Project, window: ToolWindow) {
         val contentService = ContentFactory.SERVICE.getInstance()
         val content = contentService.createContent(niddlerWindow, "Niddler", true)
+
         niddlerWindow.init()
+        niddlerWindow.onWindowVisible()
+
         window.contentManager.addContent(content)
+        window.component.addAncestorListener(object: AncestorListener{
+            override fun ancestorAdded(event: AncestorEvent?) {
+                niddlerWindow.onWindowVisible()
+            }
+
+            override fun ancestorMoved(event: AncestorEvent?) {
+                // Do nothing
+            }
+
+            override fun ancestorRemoved(event: AncestorEvent?) {
+                niddlerWindow.onWindowInvisible()
+            }
+        })
     }
 
 }
