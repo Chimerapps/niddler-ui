356f4a7692247b63fa06f5354910481359e998b2
Nicola Verbeeck <nicola@chimerapps.com>
2017-05-02 14:29:20 +0200

    Selection handling

--- niddler-ui/src/main/kotlin/com/icapps/niddler/ui/form/NiddlerWindow.kt
+++ niddler-ui/src/main/kotlin/com/icapps/niddler/ui/form/NiddlerWindow.kt
@@ -5,24 +5,17 @@ import com.icapps.niddler.ui.adb.ADBBootstrap
 import com.icapps.niddler.ui.connection.NiddlerMessageListener
 import com.icapps.niddler.ui.model.*
 import com.icapps.niddler.ui.model.messages.NiddlerServerInfo
-import com.icapps.niddler.ui.model.ui.LinkedMessagesModel
-import com.icapps.niddler.ui.model.ui.MessagesModel
-import com.icapps.niddler.ui.model.ui.TimelineMessagesTableModel
+import com.icapps.niddler.ui.model.ui.*
 import com.icapps.niddler.ui.setColumnFixedWidth
 import com.icapps.niddler.ui.util.ClipboardUtil
 import com.intellij.util.ui.tree.WideSelectionTreeUI
 import java.awt.BorderLayout
-import java.awt.Dimension
-import java.awt.Graphics
 import java.awt.datatransfer.StringSelection
 import java.awt.datatransfer.Transferable
 import java.awt.event.ItemEvent
 import java.net.URI
-import javax.accessibility.Accessible
 import javax.swing.*
 import javax.swing.border.EmptyBorder
-import javax.swing.plaf.TableUI
-import javax.swing.plaf.TreeUI
 
 /**
  * @author Nicola Verbeeck
@@ -36,6 +29,7 @@ class NiddlerWindow(interfaceFactory: InterfaceFactory, private val sdkPathGuess
     private val messagePopupMenu = NiddlerMessagePopupMenu(this)
 
     private lateinit var adbConnection: ADBBootstrap
+    private var messageMode = MessageMode.TIMELINE
 
     fun init() {
         adbConnection = ADBBootstrap(sdkPathGuesses)
@@ -55,14 +49,16 @@ class NiddlerWindow(interfaceFactory: InterfaceFactory, private val sdkPathGuess
 
             selectionModel.addListSelectionListener {
                 MainThreadDispatcher.dispatch {
-                    if (windowContents.messagesAsTable.selectedRowCount == 0) {
-                        val timer = Timer(200) {
+                    if (messageMode == MessageMode.TIMELINE) {
+                        if (windowContents.messagesAsTable.selectedRowCount == 0) {
+                            val timer = Timer(200) {
+                                checkRowSelectionState()
+                            }
+                            timer.isRepeats = false
+                            timer.start()
+                        } else {
                             checkRowSelectionState()
                         }
-                        timer.isRepeats = false
-                        timer.start()
-                    } else {
-                        checkRowSelectionState()
                     }
                 }
             }
@@ -71,6 +67,22 @@ class NiddlerWindow(interfaceFactory: InterfaceFactory, private val sdkPathGuess
             componentPopupMenu = messagePopupMenu
             model = LinkedMessagesModel()
             ui = WideSelectionTreeUI()
+
+            selectionModel.addTreeSelectionListener {
+                MainThreadDispatcher.dispatch {
+                    if (messageMode == MessageMode.LINKED) {
+                        if (windowContents.messagesAsTree.selectionCount == 0) {
+                            val timer = Timer(200) {
+                                checkRowSelectionState()
+                            }
+                            timer.isRepeats = false
+                            timer.start()
+                        } else {
+                            checkRowSelectionState()
+                        }
+                    }
+                }
+            }
         }
 
         windowContents.statusText.text = "<>"
@@ -79,23 +91,28 @@ class NiddlerWindow(interfaceFactory: InterfaceFactory, private val sdkPathGuess
         detailContainer.clear()
         windowContents.buttonClear.addActionListener {
             messages.clear()
-            val model1 = windowContents.messagesAsTable.model
-            val model2 = windowContents.messagesAsTree.model
+            if (messageMode == MessageMode.TIMELINE)
+                (windowContents.messagesAsTable.model as? MessagesModel)?.updateMessages(messages)
+            else
+                (windowContents.messagesAsTree.model as? MessagesModel)?.updateMessages(messages)
+
             windowContents.messagesAsTable.clearSelection()
+            windowContents.messagesAsTree.clearSelection()
             checkRowSelectionState()
-            if (model1 is MessagesModel)
-                model1.updateMessages(messages)
-            if (model2 is MessagesModel)
-                model2.updateMessages(messages)
         }
         windowContents.buttonTimeline.addItemListener { event ->
             if (event.stateChange == ItemEvent.SELECTED) {
                 windowContents.messagesScroller.setViewportView(windowContents.messagesAsTable)
+                messageMode = MessageMode.TIMELINE
+                (windowContents.messagesAsTable.model as? MessagesModel)?.updateMessages(messages)
+
             }
         }
         windowContents.buttonLinkedMode.addItemListener { event ->
             if (event.stateChange == ItemEvent.SELECTED) {
                 windowContents.messagesScroller.setViewportView(windowContents.messagesAsTree)
+                messageMode = MessageMode.LINKED
+                (windowContents.messagesAsTree.model as? MessagesModel)?.updateMessages(messages)
             }
         }
 
@@ -116,14 +133,31 @@ class NiddlerWindow(interfaceFactory: InterfaceFactory, private val sdkPathGuess
     }
 
     private fun checkRowSelectionState() {
-        if (windowContents.messagesAsTable.selectedRowCount == 0) {
-            detailContainer.clear()
-        } else {
-            val selectedRow = windowContents.messagesAsTable.selectedRow
-            val model = windowContents.messagesAsTable.model
-            if (model is TimelineMessagesTableModel) {
-                val row = model.getRow(selectedRow)
-                detailContainer.setMessage(row)
+        if (messageMode == MessageMode.TIMELINE) {
+            if (windowContents.messagesAsTable.selectedRowCount == 0) {
+                detailContainer.clear()
+            } else {
+                val selectedRow = windowContents.messagesAsTable.selectedRow
+                val model = windowContents.messagesAsTable.model
+                if (model is TimelineMessagesTableModel) {
+                    val row = model.getRow(selectedRow)
+                    detailContainer.setMessage(row)
+                }
+            }
+        } else if (messageMode == MessageMode.LINKED) {
+            if (windowContents.messagesAsTree.selectionCount == 0) {
+                detailContainer.clear()
+            } else {
+                val selectedPath = windowContents.messagesAsTree.selectionPath?.lastPathComponent
+                if (selectedPath is RequestNode) {
+                    val request = selectedPath.request
+                    if (request == null)
+                        detailContainer.clear()
+                    else
+                        detailContainer.setMessage(request)
+                } else if (selectedPath is ResponseNode) {
+                    detailContainer.setMessage(selectedPath.message)
+                }
             }
         }
     }
@@ -182,19 +216,21 @@ class NiddlerWindow(interfaceFactory: InterfaceFactory, private val sdkPathGuess
 
     private fun updateMessages() {
         MainThreadDispatcher.dispatch {
-            val previousSelection = windowContents.messagesAsTable.selectedRow
-            val workingModel = windowContents.messagesAsTable.model
-            val linkedModel = windowContents.messagesAsTree.model
-            if (workingModel is MessagesModel)
-                workingModel.updateMessages(messages)
-            if (linkedModel is MessagesModel)
-                linkedModel.updateMessages(messages)
-            if (previousSelection != -1) {
-                try {
-                    windowContents.messagesAsTable.addRowSelectionInterval(previousSelection, previousSelection)
-                } catch(ignored: IllegalArgumentException) {
+
+            if (messageMode == MessageMode.TIMELINE) {
+                val previousSelection = windowContents.messagesAsTable.selectedRow
+                (windowContents.messagesAsTable.model as? MessagesModel)?.updateMessages(messages)
+                if (previousSelection != -1) {
+                    try {
+                        windowContents.messagesAsTable.addRowSelectionInterval(previousSelection, previousSelection)
+                    } catch(ignored: IllegalArgumentException) {
+                    }
                 }
+            } else {
+                (windowContents.messagesAsTree.model as? MessagesModel)?.updateMessages(messages)
             }
+
+
         }
     }
 
---
+++ niddler-ui/src/main/kotlin/com/icapps/niddler/ui/model/MessageMode.kt
@@ -0,0 +1,10 @@
+package com.icapps.niddler.ui.model
+
+/**
+ * @author Nicola Verbeeck
+ * @date 02/05/2017.
+ */
+enum class MessageMode {
+    TIMELINE,
+    LINKED
+}
