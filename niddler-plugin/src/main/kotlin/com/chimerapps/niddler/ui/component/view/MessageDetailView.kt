a01799f4260238bdc41d84ad7245ca4d6c1e2d17
Nicola Verbeeck <nicola@chimerapps.com>
2019-06-07 11:07:30 +0200

    Start adding detail panel

--- niddler-plugin/src/main/kotlin/com/chimerapps/niddler/ui/component/view/MessageDetailView.kt
+++ niddler-plugin/src/main/kotlin/com/chimerapps/niddler/ui/component/view/MessageDetailView.kt
@@ -1,7 +1,38 @@
 package com.chimerapps.niddler.ui.component.view
 
+import com.icapps.niddler.lib.model.ParsedNiddlerMessage
+import java.awt.BorderLayout
+import javax.swing.JLabel
 import javax.swing.JPanel
+import javax.swing.SwingConstants
 
-class MessageDetailView : JPanel() {
+class MessageDetailView : JPanel(BorderLayout()) {
+
+    var currentMessage: ParsedNiddlerMessage? = null
+        set(value) {
+            if (field === value)
+                return
+            field = value
+            if (value == null) {
+                setEmpty()
+            } else {
+                updateUi(value)
+            }
+        }
+
+    init {
+        setEmpty()
+    }
+
+    private fun setEmpty() {
+        removeAll()
+        add(JLabel("Select a request/response", SwingConstants.CENTER), BorderLayout.CENTER)
+        revalidate()
+        repaint()
+    }
+
+    private fun updateUi(message: ParsedNiddlerMessage) {
+
+    }
 
 }
