07945f1de6da21a4a1a96d7beb52cf1d13c146ac
Nicola Verbeeck <nicola@chimerapps.com>
2019-06-12 20:01:46 +0200

    Binary renderer Form encoded renderer

--- niddler-plugin/src/main/kotlin/com/chimerapps/niddler/ui/model/renderer/BodyRenderer.kt
+++ niddler-plugin/src/main/kotlin/com/chimerapps/niddler/ui/model/renderer/BodyRenderer.kt
@@ -1,5 +1,7 @@
 package com.chimerapps.niddler.ui.model.renderer
 
+import com.chimerapps.niddler.ui.model.renderer.impl.binary.BinaryBodyRenderer
+import com.chimerapps.niddler.ui.model.renderer.impl.form.FormEncodedBodyRenderer
 import com.chimerapps.niddler.ui.model.renderer.impl.image.ImageBodyRenderer
 import com.chimerapps.niddler.ui.model.renderer.impl.json.JsonBodyRenderer
 import com.chimerapps.niddler.ui.model.renderer.impl.plain.PlainBodyRenderer
@@ -30,6 +32,8 @@ fun bodyRendererForFormat(format: BodyFormat): BodyRenderer<ParsedNiddlerMessage
         BodyFormatType.FORMAT_JSON -> JsonBodyRenderer
         BodyFormatType.FORMAT_PLAIN -> PlainBodyRenderer
         BodyFormatType.FORMAT_IMAGE -> ImageBodyRenderer
+        BodyFormatType.FORMAT_BINARY -> BinaryBodyRenderer
+        BodyFormatType.FORMAT_FORM_ENCODED -> FormEncodedBodyRenderer
         else -> null
     }
 }
---
+++ niddler-plugin/src/main/kotlin/com/chimerapps/niddler/ui/model/renderer/impl/form/FormEncodedBodyRenderer.kt
@@ -0,0 +1,106 @@
+package com.chimerapps.niddler.ui.model.renderer.impl.form
+
+import com.chimerapps.niddler.ui.component.view.PopupTable
+import com.chimerapps.niddler.ui.model.renderer.BodyRenderer
+import com.chimerapps.niddler.ui.model.renderer.reuseOrNew
+import com.chimerapps.niddler.ui.model.renderer.textAreaRenderer
+import com.chimerapps.niddler.ui.util.ui.ClipboardUtil
+import com.icapps.niddler.lib.model.ParsedNiddlerMessage
+import com.intellij.openapi.ui.JBMenuItem
+import com.intellij.openapi.ui.JBPopupMenu
+import java.awt.datatransfer.StringSelection
+import javax.swing.JComponent
+import javax.swing.JPopupMenu
+import javax.swing.JTable
+import javax.swing.table.AbstractTableModel
+
+object FormEncodedBodyRenderer : BodyRenderer<ParsedNiddlerMessage> {
+
+    override val supportsStructure: Boolean = true
+    override val supportsPretty: Boolean = true
+    override val supportsRaw: Boolean = true
+
+    @Suppress("UNCHECKED_CAST")
+    override fun structured(message: ParsedNiddlerMessage, reuseComponent: JComponent?): JComponent {
+        val data = message.bodyData as? Map<String, List<String>>
+        val component = reuseOrNew(reuseComponent) {
+            PopupTable(FormEncodedTableModel(emptyMap()), rowAtIndexCb = { model, index -> model.valueAt(index) }, popupMenuForSelectionCb = ::makePopup).also {
+                it.fillsViewportHeight = false
+                it.rowHeight = 24
+                it.showHorizontalLines = true
+                it.showVerticalLines = true
+                it.autoResizeMode = JTable.AUTO_RESIZE_ALL_COLUMNS
+                it.tableHeader = null
+
+                it.componentPopupMenu = JPopupMenu()
+            }
+        }
+        (component.second.model as FormEncodedTableModel).update(data)
+        return component.first
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun pretty(message: ParsedNiddlerMessage, reuseComponent: JComponent?): JComponent {
+        val data = message.bodyData as? Map<String, List<String>>
+        val asString = buildString {
+            data?.forEach { key, values ->
+                values.forEach { value ->
+                    append(key).append(" = ").append(value).append("\n")
+                }
+            }
+        }
+        return textAreaRenderer(asString, reuseComponent)
+    }
+
+    override fun raw(message: ParsedNiddlerMessage, reuseComponent: JComponent?): JComponent {
+        return textAreaRenderer(message.getBodyAsString(message.bodyFormat.encoding) ?: "", reuseComponent)
+    }
+
+    private fun makePopup(@Suppress("UNUSED_PARAMETER") model: FormEncodedTableModel, data: Pair<String, String>?): JPopupMenu? {
+        data ?: return null
+
+        return JBPopupMenu().also {
+            it.add(JBMenuItem("Copy key").also {
+                it.addActionListener {
+                    ClipboardUtil.copyToClipboard(StringSelection(data.first))
+                }
+            })
+            it.add(JBMenuItem("Copy value").also {
+                it.addActionListener {
+                    ClipboardUtil.copyToClipboard(StringSelection(data.second))
+                }
+            })
+        }
+    }
+}
+
+private class FormEncodedTableModel(data: Map<String, List<String>>) : AbstractTableModel() {
+
+    private var flatData = data.flatMap { (key, values) -> values.map { key to it } }
+
+    fun update(data: Map<String, List<String>>?) {
+        flatData = data?.flatMap { (key, values) -> values.map { key to it } } ?: emptyList()
+        fireTableDataChanged()
+    }
+
+    override fun getRowCount(): Int = flatData.size
+
+    override fun getColumnCount(): Int = 2
+
+    override fun getValueAt(rowIndex: Int, columnIndex: Int): Any {
+        val entry = flatData[rowIndex]
+        return if (columnIndex == 0)
+            entry.first
+        else
+            entry.second
+    }
+
+    override fun isCellEditable(rowIndex: Int, columnIndex: Int): Boolean = false
+
+    override fun getColumnClass(columnIndex: Int): Class<*> = String::class.java
+
+    fun valueAt(index: Int): Pair<String, String> {
+        return flatData[index]
+    }
+
+}
