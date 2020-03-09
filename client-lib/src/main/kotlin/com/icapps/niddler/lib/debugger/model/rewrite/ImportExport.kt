1db7d429e53180ce22ebe6d6530ac5a4100ebc6c
Nicola Verbeeck <nicola@chimerapps.com>
2020-03-09 11:27:20 +0100

    Fixed exported xml file not being compatible with charles

--- client-lib/src/main/kotlin/com/icapps/niddler/lib/debugger/model/rewrite/ImportExport.kt
+++ client-lib/src/main/kotlin/com/icapps/niddler/lib/debugger/model/rewrite/ImportExport.kt
@@ -18,6 +18,7 @@ class RewriteExporter {
 
     fun export(sets: Collection<RewriteSet>, output: OutputStream) {
         val document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument()
+        document.xmlStandalone = true
 
         val root = document.createElement("rewriteSet-array")
         document.appendChild(document.createProcessingInstruction("charles", "serialisation-version='2.0' "))
@@ -28,6 +29,7 @@ class RewriteExporter {
         val transformer = TransformerFactory.newInstance().newTransformer().apply {
             setOutputProperty(OutputKeys.ENCODING, "UTF-8")
             setOutputProperty(OutputKeys.INDENT, "yes")
+            setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, "")
             setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2")
         }
         transformer.transform(DOMSource(document), StreamResult(output))
