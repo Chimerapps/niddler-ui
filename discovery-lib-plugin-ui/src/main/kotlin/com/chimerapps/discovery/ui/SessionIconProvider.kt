7830280bb94ac8756b313e934e647c6ea776665e
Nicola Verbeeck <nicola@chimerapps.com>
2021-01-06 17:31:56 +0100

    Allow icons to be provided as base64 strings

--- discovery-lib-plugin-ui/src/main/kotlin/com/chimerapps/discovery/ui/SessionIconProvider.kt
+++ discovery-lib-plugin-ui/src/main/kotlin/com/chimerapps/discovery/ui/SessionIconProvider.kt
@@ -1,6 +1,10 @@
 package com.chimerapps.discovery.ui
 
+import com.intellij.util.IconUtil
+import java.lang.Float
+import java.util.Base64
 import javax.swing.Icon
+import javax.swing.ImageIcon
 
 interface SessionIconProvider {
 
@@ -18,6 +22,42 @@ open class DefaultSessionIconProvider : SessionIconProvider {
             "flutter" -> IncludedLibIcons.Icons.flutter
             else -> null
         }
+    }
+
+}
+
+open class Base64SessionIconProvider : SessionIconProvider {
+
+    override fun iconForString(iconString: String): Icon? {
+        try {
+            val decoded = Base64.getDecoder().decode(iconString)
+            val icon = ImageIcon(decoded)
+            if (icon.image == null)
+                return null
+
+            return IconUtil.scale(icon, null, Float.min(20.0f / icon.iconWidth, 20.0f / icon.iconHeight))
+        } catch (e: Throwable) {
+            return null
+        }
+    }
+
+}
+
+open class CompoundSessionIconProvider(vararg initialProviders: SessionIconProvider) : SessionIconProvider {
+
+    private val providers = mutableListOf<SessionIconProvider>().also {
+        it.addAll(initialProviders)
+    }
+
+    fun addProvider(sessionIconProvider: SessionIconProvider) {
+        providers += sessionIconProvider
+    }
+
+    override fun iconForString(iconString: String): Icon? {
+        providers.forEach { provider ->
+            provider.iconForString(iconString)?.let { return it }
+        }
+        return null
     }
 
 }
